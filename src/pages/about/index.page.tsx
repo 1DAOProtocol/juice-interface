69d2cb47e54a77723a25e667c6889056d3bc9143
kr1p70n1c <kr1p70n1c@gmail.com>
2023-05-15 11:02:49 +0200

    chore(seo): Updated `SEO` information to reflect `1 DAO Protocol`.

--- src/pages/about/index.page.tsx
+++ src/pages/about/index.page.tsx
@@ -5,7 +5,7 @@ export default function AboutPage() {
   return (
     <>
       <Head
-        title="About Juicebox | Crypto Fundraising & DAO Management"
+        title="About 1 DAO Protocol | Crypto Fundraising & DAO Management"
         overrideFormattedTitle
         url={process.env.NEXT_PUBLIC_BASE_URL + '/about'}
         description="Juicebox is the programmable funding platform for crypto and web3. Fund, operate, and scale your project transparently. Community DAO owned, on Ethereum."
